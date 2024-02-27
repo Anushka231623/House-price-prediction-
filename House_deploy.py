086c03fcbd65ce8a41e9fc72e7118589d31c1a91
Anushka231623 <138510615+Anushka231623@users.noreply.github.com>
2024-02-27 15:21:46 +0530

    Update House_deploy.py

--- House_deploy.py
+++ House_deploy.py
@@ -1,50 +1,58 @@
 import pickle
 import numpy as np
 import streamlit as st
-import joblib
 import requests
+import joblib
 
-# Function to load the model
-def load_model():
- model_url = "https://raw.githubusercontent.com/Anushka231623/House-price-prediction-/blob/main/finalized_model.sav"
- r = requests.get(model_url)
- if r.status_code == 200:
-  with open('finalized_model.sav', 'wb') as f:
-   f.write(r.content)
-   loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
-   return loaded_model
- else:
-  print("Failed to download the model file")
-  return None
-  # Load the model
-loaded_model = load_model()
-# Function to perform prediction
+# Load the model
+#loaded_model = pickle.load(open(r"https://raw.githubusercontent.com/ManasiBhavsar/sayali-house-price/blob/main/sayali_model.sav", 'rb'))
+model_url="https://raw.githubusercontent.com/Anushka231623/House-price-prediction-/blob/main/finalized_model.sav"
+r=requests.get(model_url)
+
+if r.status_code==200:
+    with open('finalized_model.sav','wb') as f:
+        f.write(r.content)
+else:
+    print("Failed to download the model file")
+    
+model = joblib.load('finalized_model.sav')
+
 def DecisionTreeRegressor(input_data):
     input_data_asarray = np.asarray(input_data)
     input_data_reshaped = input_data_asarray.reshape(1, -1) 
-    prediction = loaded_model.predict(input_data_reshaped)
+    prediction = model.predict(input_data_reshaped)
     return prediction
- def predict_price(entries):
-  try:
-   # Get user input
-   input_data = [int(entries[0]), float(entries[1]), int(entries[2]), int(entries[3]), 
+
+def predict_price(entries):
+    try:
+        # Get user input
+        input_data = [int(entries[0]), float(entries[1]), int(entries[2]), int(entries[3]), 
                       float(entries[4]), int(entries[5]), int(entries[6]), int(entries[7]), 
                       int(entries[8]), int(entries[9]), int(entries[10]), int(entries[11])]
-   # Perform prediction
-   predicted_price = DecisionTreeRegressor(input_data)[0]
-   return f"The predicted price is ${predicted_price:,.2f}"
-  except ValueError:
-   return "Please enter valid inputs."
-   def main():
+        
+        # Perform prediction
+        predicted_price = DecisionTreeRegressor(input_data)[0]
+        return f"The predicted price is ${predicted_price:,.2f}"
+    except ValueError:
+        return "Please enter valid inputs."
+
+        
+ 
+
+
+def main():
     st.title("House Price Prediction")
     entries = []
     for feature in ['Bedrooms:', 'Bathrooms:', 'Sqft Living:', 'Sqft Lot:', 'Floors:', 
                     'Waterfront:', 'View:', 'Condition:', 'Sqft Above:', 'Sqft Basement:', 
                     'Year Built:', 'Year Renovated:']:
-                     entries.append(st.number_input(feature))
-                     if st.button('Predict Price'):
-                      result = predict_price(entries)
-                      st.success(result)
-                      if __name__ == '__main__':
-                       main()
+        entries.append(st.number_input(feature))
+    
+    if st.button('Predict Price'):
+        result = predict_price(entries)
+        st.success(result)
+
+if __name__ == '__main__':
+    main()
+
 
